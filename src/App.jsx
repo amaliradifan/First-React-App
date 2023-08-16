0d0d88b785f13b76fb9d7b52d927fde8d2ea3a13
Radifan <amaliradifan9a@gmail.com>
2023-08-16 12:33:27 +0700

    first use props

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greet from "./greet";
+import Name from "./Name";
+
+function App() {
+	return (
+		<div>
+			<Greet name="Dipan" />
+			<Greet name="Radif" />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Name.jsx
@@ -0,0 +1,4 @@
+import "./Name.css";
+export default function Name() {
+	return <h1 className="Name">Amali Radifan</h1>;
+}
