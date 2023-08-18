234b16de647ef23f55edf086a6be330238d0647d
Radifan <amaliradifan9a@gmail.com>
2023-08-18 14:10:28 +0700

    Menambah multiple props dengan tipe non string props

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Name from "./Name";
 function App() {
 	return (
 		<div>
-			<Greet name="Dipan" />
-			<Greet name="Radif" />
-			<Greet name="Fikri" />
+			<Greet name="Dipan" from="Mama" order={1} />
+			<Greet name="Radif" from="Papa" order={2} />
+			<Greet name="Fikri" from="Dedek" order={3} />
 		</div>
 	);
 }
--- src/Greet.jsx
+++ src/Greet.jsx
@@ -1,5 +1,13 @@
-function Greet({ name }) {
-	return <h1>Halloo!! {name}!!! </h1>;
+function Greet({ name, from, order }) {
+	const get = order * 10000;
+	return (
+		<>
+			<h1>
+				Halloo!! {name}!!! You Get ${get}
+			</h1>
+			<h2>- {from}</h2>
+		</>
+	);
 }
 
 export default Greet;
